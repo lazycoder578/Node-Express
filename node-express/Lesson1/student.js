45b634a3fabc26f084432335e6399fca0d591d57
Md. Abdul Kader <76205773+lazycoder578@users.noreply.github.com>
2023-01-09 00:59:06 +0600

    Add files via upload

---
+++ node-express/Lesson1/student.js
@@ -0,0 +1,19 @@
+exports.getName = () =>{
+    return "Abdul Kader";
+}
+
+const getAge = () => {
+    return "23";
+}
+
+const cgpa = 3.92;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+// module.exports = {
+//     getName,
+//     getAge,
+//     cgpa
+// }
